957a422687df0c89c2f1bc3301a3da30743d9a95
danish <danishofcodes@gmail.com>
2024-06-17 01:12:04 +0530

    dummydata sets in js array of objects form

---
+++ src/data/dummydata.js
@@ -0,0 +1,59 @@
+export const questions = [
+  {
+    id: 1,
+    feedbackquestion: "Are you happy with SurveyBubble overall?",
+    type: "emojireact",
+    name: 'happyornot',
+    options: ["Loved it", "Soo good", "Okay", "Not good", "Super Bad"]
+  },
+
+  {
+    id: 2,
+    feedbackquestion: "Which features are most valuable to you?",
+    type: "checkbox",
+    name: 'valuablefeatures',
+    options: ["Create Survey Form in Instant", "Analytics and User Trends", "Affordable Subscription Plan", "No disturbing adds on Page"],
+  },
+
+  {
+    id: 3,
+    feedbackquestion: " How likely are you to recommend SurveyBubble to a work buddy, collegue or friends?",
+    type: "radio",
+    name: 'willrecommend',
+    options: ["Strongly Recommend", "Very likely to Recommend", "Maybe", "No not at all"],
+  },
+
+  {
+    id: 4,
+    feedbackquestion: "Which domains of work do you usually use SurveyBubble for?",
+    type: "checkbox",
+    name: 'domainaffecting',
+    options: ["Getting Feedback From Students", "Getting feedback from clients/customers", "Getting Feedbacks from Employees", "Market Research and Opinion Polls", "Product or Service Concept Testing"],
+  },
+
+  {
+    id: 5,
+    feedbackquestion: "What important features are we missing?",
+    type: "textarea",
+    name: 'missingfeatures',
+    options: null,
+  },
+  // { id: 6, 
+  //   feedbackquestion: "Are you satisfied?",
+  //   type: "radio",
+  //   name:'satisfied',
+  //   options: ["happy", "yeah okay", "not that much", "yuck"]
+  // },
+  // { id: 7, 
+  //   feedbackquestion: "Are you happy?",
+  //   type: "emojireact",
+  //   name:'happyornot',
+  //   options: ["Loved it", "Soo good", "Okay", "Not good", "Super Bad"]
+  // },
+  // { id: 8, 
+  //   feedbackquestion: "Does it work properly?",
+  //   type: "checkbox",
+  //   name:'checkboxfeedback2',
+  //   options: ["option x", "option y", "option z", "option zxy"]
+  // },
+];
