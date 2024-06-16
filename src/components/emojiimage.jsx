fc463a2578f897bc600957422de9c5e5696d13e3
danish <danishofcodes@gmail.com>
2024-06-17 00:58:37 +0530

    file importing emoji images

---
+++ src/components/emojiimage.jsx
@@ -0,0 +1,14 @@
+import lovedit from '../assets/images/lovedit.png';
+import feelssogood from '../assets/images/feelssogood.png';
+import good from '../assets/images/good.png';
+import nahnotgood from '../assets/images/nahnotgood.png';
+import superbad from '../assets/images/superbad.png';
+
+export const emojiIcons = {
+    lovedit: lovedit,
+    soogood: feelssogood,
+    okay: good,
+    notgood: nahnotgood,
+    superbad: superbad
+};
+
