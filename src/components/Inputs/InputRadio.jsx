2abeb0b56cc9bf8c4d6dc5b3ab9de35b46e52dd5
danishofcodes <161294358+danishofcodes@users.noreply.github.com>
2024-06-18 15:57:08 +0530

    Update InputRadio.jsx

--- src/components/Inputs/InputRadio.jsx
+++ src/components/Inputs/InputRadio.jsx
@@ -10,7 +10,7 @@ export default function InputRadio({ option, name, handleRadioChange , formData}
  const  isChecked = option==formData[name];
   return (
     <div className='flex items-center py-2'>
-     <label  className={`w-full flex items-center  px-2 rounded-lg ${isChecked? "text0492af] bg-[#c0e7f0] text-[#0492af]"  : ''}`} >
+     <label  className={`w-full flex items-center py-2 px-2 rounded-lg ${isChecked? "text-[#0492af] bg-[#c0e7f0]"  : ''}`} >
       <input type="radio" name={name} value={option} onChange={handleRadioChange} className='me-2' checked={isChecked}/>
       <span>{option}</span></label>
     </div>
