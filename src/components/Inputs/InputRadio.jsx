b91875322942ddd821aeb2be1637f1d961cac215
danishofcodes <161294358+danishofcodes@users.noreply.github.com>
2024-06-19 11:55:09 +0530

    padding issue solved

--- src/components/Inputs/InputRadio.jsx
+++ src/components/Inputs/InputRadio.jsx
@@ -9,7 +9,7 @@ export default function InputRadio({ option, name, handleRadioChange , formData}
   // console.log("see if equal ", option==formData[name])
  const  isChecked = option==formData[name];
   return (
-    <div className='flex items-center py-2'>
+    <div className='flex items-center py-1'>
      <label  className={`w-full flex items-center py-2 px-2 rounded-lg ${isChecked? "text-[#0492af] bg-[#c0e7f0]"  : ''}`} >
       <input type="radio" name={name} value={option} onChange={handleRadioChange} className='me-2' checked={isChecked}/>
       <span>{option}</span></label>
